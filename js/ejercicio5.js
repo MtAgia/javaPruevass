e99983ed442a5741695935bcb20fa7094f91e16c
Matias G <dikimatig@gmail.com>
2023-03-01 05:43:51 -0300

    ejercicio 5 terminado

---
+++ js/ejercicio5.js
@@ -0,0 +1,6 @@
+let num1 = prompt("ponga numero 1 para la suma");
+let num2 = parseInt(num1)
+let num3 = prompt("ponga numero 2 para la suma");
+let num4 = parseInt(num3)
+let resultado = num2 + num4
+document.write("la suma de estos numeros da como resultado: " + resultado)
