b5fdcf1d4e693c0979d08d7a33d291fc79402039
Matias G <dikimatig@gmail.com>
2023-03-02 01:32:09 -0300

    ejercicio 8 terminado

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("ingrese un numero: "))
+
+if(num1 % 2 === 0){
+    document.write("el numero " + num1 + " es divisible por 2")
+}else{
+    document.write("no fue divisible por 2")
+}
